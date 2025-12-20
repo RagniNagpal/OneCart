4aa8cb1ae8b917e5f556928e4620ac88471fe687
RagniNagpal <ragninagpal@gmail.com>
2025-12-20 22:02:07 +0530

    token + cookie

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,8 +1,9 @@
 import User from "../model/userModel.js";
 import validator from "validator"
 import bcrypt from "bcryptjs"
+import { genToken } from "../config/token.js"
 
-export const register = async(req,res)=>{
+export const registration = async(req,res)=>{
   try{
     const {name,email,password}=req.body;
     const existUser = await User.findOne({email});
@@ -12,12 +13,21 @@ export const register = async(req,res)=>{
     if(validator.isEmail(email)){
       return res.status(400).json({message:"Enter Valid Email"})
     }
-    if(password.length < 0){
+    if(password.length < 8){
       return res.status(400).json({message:"Enter Strong Password"})
     }
     let hashPassword = await bcrypt.hash(password,10)
     const user=await User.create({name,email,password : hashPassword})
+    let token = await genToken(user._id)
+    res.cookie(("token",token,{
+      httpOnly:true,
+      secure:false,
+      sameSite: "Strict",
+      maxAge : 7*24*60*60*1000
+    }))
+    return res.status(201).json(user)
     } catch(error){
-
+      console.log("register error")
+      return res.status(500).json({message : `Registeration error ${error}` })
     }
 }
