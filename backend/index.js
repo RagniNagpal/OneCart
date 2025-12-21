97b0faf4a84eb6e4d921fea0654b1f5f99325c8d
RagniNagpal <ragninagpal@gmail.com>
2025-12-21 18:34:11 +0530

    Server

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDb from './config/db.js';
+import cookieParser from 'cookie-parser'
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 let port=process.env.PORT || 3000;
 
 let app=express();
 
-app.listen(port,()=>{
-  console.log("Hello from server")
-  connectDb()
-})
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth",authRoutes)
+
+// app.listen(port,()=>{
+//   console.log("Hello from server")
+//   connectDb()
+// })
+
+connectDb().then(() => {
+  app.listen(port, () => console.log(`🚀 Server running on port ${port}`));
+});
