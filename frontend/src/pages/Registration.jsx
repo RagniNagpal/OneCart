8ae2a93eadc044c6e56a3acd39beb175c6947913
RagniNagpal <ragninagpal@gmail.com>
2025-12-25 22:13:27 +0530

    logic done, auth ui done

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import Logo from "../assets/vcart-logo.png";
 import { useNavigate } from 'react-router-dom';
 import google from "../assets/google.png"
+import { IoEyeOutline } from "react-icons/io5";
+import { IoEye } from "react-icons/io5";
+import { useContext } from 'react';
+import { AuthDataContext } from '../context/authContext';
+import axios from 'axios'
 
 function Registration() {
+  let [show,setShow] = useState(false);
+  let {serverUrl} = useContext(AuthDataContext);
+  let [name,setName] = useState("")
+  let [email,setEmail] = useState("")
+  let [password,setPassword] = useState("")
+
   let navigate = useNavigate()
+
+  const handleSignup = async (e) => {
+  e.preventDefault();
+
+  try {
+    const result = await axios.post(
+      serverUrl + "/api/auth/registration",
+      { name, email, password },
+      { withCredentials: true }
+    );
+
+    console.log(result.data);
+
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
   return (
     <div className='w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-[white] flex flex-col items-center justify-start'>
       <div className='w-[100%] h-[80px] flex items-center justify-start px-[30px] gap-[10px] cursor-pointer' onClick={() =>navigate("/")}>
@@ -17,7 +47,7 @@ function Registration() {
       </div>
       <div className='max-w-[600px] w-[90%] h-[500px] bg-[#00000025] border-[1px] border-[#96969635] backdrop-blur-2xl rounded-lg shadow-lg flex items-center justify-center'>
 
-        <form action="" className='w-[90%] h-[90%] flex flex-col items-center justify-start gap-[20px]'>
+        <form action="" onSubmit={handleSignup} className='w-[90%] h-[90%] flex flex-col items-center justify-start gap-[20px]' >
           <div className='w-[98%] h-[50px] bg-[#42656cae] rounded-lg flex items-center justify-center gap-[18px] py-[20px] cursor-pointer'>
             <img src={google} alt="" className='w-[20px]'/>Registration with Google
           </div>
@@ -25,11 +55,26 @@ function Registration() {
           <div className='w-[40%] h-[1px] bg-[#96969635]'></div>
           <span>OR</span>
           <div className='w-[40%] h-[1px] bg-[#96969635]'></div>
-          <div className='w-[90%] h-[400px] flex flex-col items-center justify-center gap-[15px]'>
-            <input type="text" className='w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop-blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold ' placeholder='UserName' required />
-            <input type="text" className='w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop-blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold ' placeholder='Email' required />
-            <input type="text" className='w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop-blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold ' placeholder='Password' required />
           </div>
+          <div className='w-[90%] h-[400px] flex flex-col items-center justify-center gap-[15px] relative'>
+            <input type="text" className='w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop-blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold ' placeholder='UserName' required onChange={(e) => setName(e.target.value)} value={name} />
+            <input type="text" className='w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop-blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold ' placeholder='Email' required onChange={(e) => setEmail(e.target.value)} value={email}/>
+            <input type={show ? "text" : "password"} className='w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop-blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold ' placeholder='Password' required onChange={(e) => setPassword(e.target.value)} value={password}/>
+            {show ? (
+                <IoEye
+                  className="w-[20px] h-[20px] cursor-pointer absolute right-[5%]"
+                  onClick={() => setShow(false)}
+                />
+              ) : (
+                <IoEyeOutline
+                  className="w-[20px] h-[20px] cursor-pointer absolute right-[5%]"
+                  onClick={() => setShow(true)}
+                />
+              )}
+
+          <button className='w-[100%] h-[50px] bg-[#6060f5] rounded-lg flex items-center justify-center mt-[20px] text-[17px] font-semibold'>Create Account</button>
+          
+          <p className='flex gap-[10px]'>You have any account? <span className='text-[#5555f6cf] text-[17px] font-semibold cursor-pointer' onClick={ ()=> navigate("/login")}>Login</span></p>
           </div>
         </form>
       </div>
